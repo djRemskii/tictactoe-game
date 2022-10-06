d7aa9b9083588540ce871f82a4f118be8a7c086e
djRemskii <jeremyong@cpp.edu>
2022-10-05 19:25:20 -0700

    created tictactoe.c

---
+++ tictactoe.c
@@ -0,0 +1,22 @@
+//Tic Tac Toe game between 2 players or playervscomputer
+
+int main(){
+
+    //prompt user for game they wish to play (vs human or vs computer)
+
+    /* functions needed:
+        1. spot select
+        2. check win
+        3. reset
+        4. display board
+    */
+
+   
+
+    //read information from console
+
+    //asfasdf
+
+
+    return 0;
+}
