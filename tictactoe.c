553df5aeef0f73de157de4c1f8eafa6af95d470a
djRemskii <jeremyong@cpp.edu>
2022-10-06 11:47:37 -0700

    started to created needed logic functions

--- tictactoe.c
+++ tictactoe.c
@@ -1,8 +1,31 @@
 //Tic Tac Toe game between 2 players or playervscomputer
+#include <stdio.h>
+#include <stdbool.h>
+
+int grid[3][3]; 
+
+void reset();
+bool spotSelect();
+bool checkWin();
+void displayBoard();
 
 int main(){
 
+    
+    reset();
+
     //prompt user for game they wish to play (vs human or vs computer)
+    printf("Please choose the type of game (input number):\n\t1 - vs human\n\t2 - vs computer\n");
+
+    int gameType;
+    gameType = getchar();
+
+    putchar(gameType);
+
+    
+    
+
+    
 
     /* functions needed:
         1. spot select
@@ -19,4 +42,22 @@ int main(){
 
 
     return 0;
+}
+
+void reset (){
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                grid[i][j] = 0;
+            }
+        }
+        printf("grid reset");
+}
+
+bool spotSelect(int row, int column, int player){
+    if (grid[row][column] == 0){
+        grid[row][column] = player;
+        return true;
+    } else {
+        return false;
+    }
 }
